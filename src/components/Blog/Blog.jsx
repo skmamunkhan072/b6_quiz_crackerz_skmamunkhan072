5851715e9f9b9b2aba5dc54c751f0b9e1f1bc48a
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-10-12 22:23:28 +0600

    three qustion ans add

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,20 +3,20 @@ import BlogCart from "../BlogCart/BlogCart";
 import "./Blog.css";
 
 const Blog = () => {
-  const data = [
+  const datas = [
     {
       id: 1,
-      quistion: "what is the purpose of react router ?",
+      quistion: "What is the purpose of react router ?",
       ans: "ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.",
     },
     {
       id: 2,
-      quistion: "",
-      ans: "",
+      quistion: "What is useref ?",
+      ans: "Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with <div ref={myRef} /> , React will set its .current property to the corresponding DOM node whenever that node changes.",
     },
     {
       id: 3,
-      quistion: "how does context api work ?",
+      quistion: "How does context api work ?",
       ans: 'The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.',
     },
   ];
@@ -27,9 +27,9 @@ const Blog = () => {
         className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-20 shadow-md rounded-xl"
       >
         <div className="grid gap-8 row-gap-5 mb-8 md:row-gap-8 lg:grid-cols-2 sm:grid-cols-1">
-          <BlogCart />
-          <BlogCart />
-          <BlogCart />
+          {datas.map((data) => (
+            <BlogCart key={data.id} data={data} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/BlogCart/BlogCart.jsx
+++ src/components/BlogCart/BlogCart.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-const BlogCart = () => {
+const BlogCart = ({ data }) => {
   return (
     <div className="duration-300 transform border-sky-700 shadow-md 	 rounded-xl bg-black border-2 border-deep-purple-accent-400 hover:-translate-y-2">
+      <h1 className="mt-4 text-2xl">
+        {data.quistion ? data.quistion : "No Data Found"}
+      </h1>
       <div className="h-full p-5 rounded-r shadow-sm">
-        <p className="text-sm mb-4">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
-          illum aut veritatis vel repellendus sed quibusdam commodi in veniam.
-          Maiores dignissimos architecto quaerat quae, sed aut perferendis
-          maxime esse consequuntur?
-        </p>
+        <p className="text-sm mb-4">{data.ans ? data.ans : "No Data Found"}</p>
       </div>
     </div>
   );
